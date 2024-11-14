b18c0925b588940c537248894b614d6b21550634
Ronan Vieira do Carmo Junior <ronanvcjunior@gmail.com>
2024-11-14 15:31:43 -0300

    :sparkles: feat: adiciona entidade TaskEntity com campos relacionados à tarefa e vínculo com o usuário

---
+++ src/main/java/com/ronanvcjunior/taskmaster/entities/TaskEntity.java
@@ -0,0 +1,46 @@
+package com.ronanvcjunior.taskmaster.entities;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.ZonedDateTime;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_DEFAULT;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tasks")
+@JsonInclude(NON_DEFAULT)
+public class TaskEntity extends  Auditable {
+
+    @Column(updatable = false, unique = true, nullable = false)
+    private String taskId;
+
+    @Column(nullable = false)
+    private String name;
+
+    private BigDecimal cost;
+
+    @Column(name = "payment_deadline")
+    private ZonedDateTime paymentDeadline;
+
+    @Column(unique = true, nullable = false)
+    private Integer order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(
+            name = "user_id",
+            referencedColumnName = "id",
+            foreignKey = @ForeignKey(name = "fk_documents_owner", foreignKeyDefinition = "foreign key /* FK */ (user_id) references UserEntity", value = ConstraintMode.CONSTRAINT)
+    )
+    private UserEntity owner;
+
+
+}
