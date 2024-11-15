957477ce04d58e6116af6f5ce7b2c646c9779e28
Ronan Vieira do Carmo Junior <ronanvcjunior@gmail.com>
2024-11-15 10:58:50 -0300

    :sparkles: feat: adicionar método para conversão de UserEntity em User e verificação de credenciais

--- src/main/java/com/ronanvcjunior/taskmaster/constants/SecurityConstants.java
+++ src/main/java/com/ronanvcjunior/taskmaster/constants/SecurityConstants.java
@@ -2,11 +2,18 @@ package com.ronanvcjunior.taskmaster.constants;
 
 public class SecurityConstants {
     public static final Integer STRENGTH = 12;
+    public static final int NINETY_DAYS = 90;
+
     public static final String BASE_PATH = "/**";
     public static final String[] PUBLIC_URLS = {
             "/user/register/**",
             "/user/verify/account/**",
             "/user/login/**"
+    };
+    public static final String [] PUBLIC_ROUTES = {
+            "/user/register",
+            "/user/verify/account",
+            "/user/login"
     };
 
 }
