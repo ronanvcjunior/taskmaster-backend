36b2cec43420bef4f4fa34f6d617675ad75d63a8
Ronan Vieira do Carmo Junior <ronanvcjunior@gmail.com>
2024-11-15 00:45:31 -0300

    :sparkles: feat: integrar serviços de credenciais e confirmação no registro de usuário

--- src/main/java/com/ronanvcjunior/taskmaster/services/implementations/UserServiceImpl.java
+++ src/main/java/com/ronanvcjunior/taskmaster/services/implementations/UserServiceImpl.java
@@ -1,13 +1,18 @@
 package com.ronanvcjunior.taskmaster.services.implementations;
 
 import com.ronanvcjunior.taskmaster.domains.RequestContext;
+import com.ronanvcjunior.taskmaster.entities.ConfirmationEntity;
+import com.ronanvcjunior.taskmaster.entities.CredentialEntity;
 import com.ronanvcjunior.taskmaster.entities.RoleEntity;
 import com.ronanvcjunior.taskmaster.entities.UserEntity;
 import com.ronanvcjunior.taskmaster.repositories.UserRepository;
+import com.ronanvcjunior.taskmaster.services.ConfirmationService;
+import com.ronanvcjunior.taskmaster.services.CredentialService;
 import com.ronanvcjunior.taskmaster.services.RoleService;
 import com.ronanvcjunior.taskmaster.services.UserService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import static com.ronanvcjunior.taskmaster.enums.Authority.*;
@@ -20,6 +25,8 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
 
     private final RoleService roleService;
+    private final CredentialService credentialService;
+    private final ConfirmationService confirmationService;
 
     @Override
     public void createUser(String firstName, String lastName, String email, String password) {
@@ -27,6 +34,10 @@ public class UserServiceImpl implements UserService {
 
         UserEntity userEntity = this.createNewUser(firstName, lastName, email);
         this.userRepository.save(userEntity);
+
+        this.credentialService.createCredential(userEntity, password);
+
+        this.confirmationService.createConfirmation(userEntity);
     }
 
     private UserEntity createNewUser(String firstName, String lastName, String email) {
