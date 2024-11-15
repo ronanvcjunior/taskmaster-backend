324a8b974b6816523f813ce72df032755591c988
Ronan Vieira do Carmo Junior <ronanvcjunior@gmail.com>
2024-11-15 10:55:57 -0300

    :sparkles: feat: adicionar método para buscar credenciais por ID de usuário

--- src/main/java/com/ronanvcjunior/taskmaster/services/CredentialService.java
+++ src/main/java/com/ronanvcjunior/taskmaster/services/CredentialService.java
@@ -1,7 +1,10 @@
 package com.ronanvcjunior.taskmaster.services;
 
+import com.ronanvcjunior.taskmaster.entities.CredentialEntity;
 import com.ronanvcjunior.taskmaster.entities.UserEntity;
 
 public interface CredentialService {
     void createCredential(UserEntity userEntity, String password);
+
+    CredentialEntity getUserCredentialByUserId(String userId);
 }
--- src/main/java/com/ronanvcjunior/taskmaster/services/implementations/CredentialServiceImpl.java
+++ src/main/java/com/ronanvcjunior/taskmaster/services/implementations/CredentialServiceImpl.java
@@ -2,6 +2,7 @@ package com.ronanvcjunior.taskmaster.services.implementations;
 
 import com.ronanvcjunior.taskmaster.entities.CredentialEntity;
 import com.ronanvcjunior.taskmaster.entities.UserEntity;
+import com.ronanvcjunior.taskmaster.exceptions.ApiException;
 import com.ronanvcjunior.taskmaster.repositories.CredentialRepository;
 import com.ronanvcjunior.taskmaster.services.CredentialService;
 import jakarta.transaction.Transactional;
@@ -9,10 +10,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @Transactional(rollbackOn = Exception.class)
 @RequiredArgsConstructor
 public class CredentialServiceImpl implements CredentialService {
+    public static final String CREDENTIAL_NOT_FOUND = "Credential não encontrado";
+
     private final CredentialRepository credentialRepository;
 
     private final BCryptPasswordEncoder encoder;
@@ -21,5 +26,11 @@ public class CredentialServiceImpl implements CredentialService {
     public void createCredential(UserEntity userEntity, String password) {
         CredentialEntity credentialEntity = new CredentialEntity(userEntity, encoder.encode(password));
         this.credentialRepository.save(credentialEntity);
+    }
+
+    @Override
+    public CredentialEntity getUserCredentialByUserId(String userId) {
+        Optional<CredentialEntity> credentialEntity = this.credentialRepository.findCredentialEntityByUserUserId(userId);
+        return credentialEntity.orElseThrow(() -> new ApiException(CREDENTIAL_NOT_FOUND));
     }
 }
