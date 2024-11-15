80329054e0fa8cc96027e7e0f641b761ea93b191
Ronan Vieira do Carmo Junior <ronanvcjunior@gmail.com>
2024-11-15 12:13:33 -0300

    :sparkles: feat: adicionar lógica de logout com remoção de cookies JWT no `ApiLogoutHandler`

--- src/main/java/com/ronanvcjunior/taskmaster/contollers/UserController.java
+++ src/main/java/com/ronanvcjunior/taskmaster/contollers/UserController.java
@@ -2,11 +2,14 @@ package com.ronanvcjunior.taskmaster.contollers;
 
 import com.ronanvcjunior.taskmaster.dtos.Response;
 import com.ronanvcjunior.taskmaster.dtos.requests.UserRequest;
+import com.ronanvcjunior.taskmaster.handlers.ApiLogoutHandler;
 import com.ronanvcjunior.taskmaster.services.UserService;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
@@ -22,6 +25,8 @@ import static org.springframework.http.HttpStatus.OK;
 public class UserController {
     private final UserService userService;
 
+    private final ApiLogoutHandler logoutHandler;
+
     @PostMapping("/register")
     public ResponseEntity<Response> saveUser(@RequestBody @Valid UserRequest user, HttpServletRequest request) {
         userService.createUser(user.firstName(), user.lastName(), user.email(), user.password());
@@ -33,7 +38,14 @@ public class UserController {
     public ResponseEntity<Response> verifyUser(@RequestParam("key") @Valid String key, HttpServletRequest request) {
         userService.verifyAccountKey(key);
 
-        return ResponseEntity.ok().body(getResponse(request, emptyMap(), "Account verified", OK));
+        return ResponseEntity.ok().body(getResponse(request, emptyMap(), "Conta Verificada", OK));
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Response> logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        logoutHandler.logout(request, response, authentication);
+
+        return ResponseEntity.ok().body(getResponse(request, emptyMap(), "Você efetuou logout com sucesso", OK));
     }
 
 }
